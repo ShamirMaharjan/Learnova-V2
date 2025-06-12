351c851545c5c6e6e5b3fe2ba026e80d3d4cb8c4
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-12 13:03:12 +0545

    create auth ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Signinview from '@/modules/auth/ui/views/sign-in-view'
+import { Card } from '@/components/ui/card'
+
+const page = () => {
+    return (
+        <Signinview />
+
+
+    )
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Card } from '@/components/ui/card'
+import Signupview from '@/modules/auth/ui/views/Sign-up-view'
+const page = () => {
+    return (
+        <Signupview />
+    )
+}
+
+export default page
