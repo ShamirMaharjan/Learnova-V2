70d05b8612a3d1c3686198cd9dd00d1af90cedfe
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-22 08:37:59 +0545

    implement meeting data table UI and logic

--- src/modules/meetings/ui/views/Meetings-View.tsx
+++ src/modules/meetings/ui/views/Meetings-View.tsx
@@ -1,16 +1,18 @@
 "use client"
 
+import { DataTable } from "@/components/Data-Table";
 import ErrorState from "@/components/Error-State";
 import LoadingState from "@/components/Loading-State";
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
 
 export const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
     return (
-        <div>
-            {JSON.stringify(data)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data.items} columns={columns} />
         </div>
     )
 }
